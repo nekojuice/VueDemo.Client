25e0b58115182e579b3d7a2570219640be42c336
Jose Wu <josewu@uictech.com.tw>
2024-05-28 14:28:21 +0800

    update: layout標記框線

--- src/views/LayoutView.vue
+++ src/views/LayoutView.vue
@@ -2,13 +2,13 @@
 import { RouterView } from 'vue-router'
 </script>
 <template>
-  <div class="container">
+  <div class="container border border-primary">
     <RouterView name="Header" />
-    <div class="row">
-      <RouterView class="col-3" name="NavBar" />
-      <RouterView class="col-9" />
+    <div class="row border border-danger">
+      <RouterView class="col-3 border border-info" name="NavBar" />
+      <RouterView class="col-9 border border-warning" />
     </div>
-    <RouterView class="row" name="Footer" />
+    <RouterView class="row border border-primary" name="Footer" />
   </div>
 </template>
 
